736ae3a511f681ddd895bb49bfe154cecb4ab8fd
Aleksey <AlekseyNikitchenko@users.noreply.github.com>
2020-11-09 20:14:42 +0200

    Update test_data_preprocessor.py

--- lab2/tests/test_data_preprocessor.py
+++ lab2/tests/test_data_preprocessor.py
@@ -1,24 +1,5 @@
 import numpy as np
-
-
-def data_preprocessor(x, label=None):
-    """
-    Concatenate bias vector and labels to original x
-    :param x: generated sample from multivariate normal distribution
-    :param label: label(target) vector
-    :return: merged data
-    """
-    n, m = x.shape
-
-    new_features = np.array([x[:, i] * x[:, j] for i in range(m) for j in range(m)]).T
-    bias = np.ones(shape=(n, 1))
-
-    if label is None:
-        return np.concatenate((new_features, x, bias), axis=1)
-    else:
-        target = label * np.ones(shape=(n, 1))
-        return np.concatenate((new_features, x, bias, target), axis=1)
-
+from ..ellipse import data_preprocessor
 
 def test_data_preprocessor():
 
