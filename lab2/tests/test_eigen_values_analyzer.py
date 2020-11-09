eef7d9cde721d9fd0c4a1154bbe9e44a7ac977a1
Aleksey <AlekseyNikitchenko@users.noreply.github.com>
2020-11-09 20:16:10 +0200

    Update test_eigen_values_analyzer.py

--- lab2/tests/test_eigen_values_analyzer.py
+++ lab2/tests/test_eigen_values_analyzer.py
@@ -1,46 +1,5 @@
 import numpy as np
-
-
-def eigen_values_analyzer(weights):
-    """
-    Check that the first values of weights that form positive definite matrix have
-    positive eigen values.  If exists even one non-positive eigen-value - function
-    return an additional vector
-    :param weights:
-    :return: additional vector if exists even one non-positive eigen-value, else return None
-    """
-    matrix = weights[:4].reshape((2, 2))
-    n = matrix.shape[0]
-    eig_values, eig_vectors = np.linalg.eig(matrix)
-
-    for i in range(n):
-        if eig_values[i] <= 0:
-
-            eta = data_preprocessor(eig_vectors[:, i].reshape(1, -1))[0]
-            eta[-3:] = 0
-            return eta
-    return None
-
-
-def data_preprocessor(x, label=None):
-    """
-    Concatenate bias vector and labels to original x
-    :param x: generated sample from multivariate normal distribution
-    :param label: label(target) vector
-    :return: merged data
-    """
-    n, m = x.shape
-
-    new_features = np.array([x[:, i] * x[:, j] for i in range(m) for j in range(m)]).T
-    bias = np.ones(shape=(n, 1))
-
-    if label is None:
-        return np.concatenate((new_features, x, bias), axis=1)
-    else:
-        target = label * np.ones(shape=(n, 1))
-        return np.concatenate((new_features, x, bias, target), axis=1)
-
-
+from ..ellipse import eigen_values_analyzer
 def test_eigen_values_analyzer():
 
     assert eigen_values_analyzer(np.array([1, 0, 0, 1, 1, 1, 0])) is None
