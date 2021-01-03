97b50dbd83f515f5dfa8a6f081bf244474eaa95b
Leonid Popryho <18726515+popryho@users.noreply.github.com>
2021-01-03 16:18:01 +0200

    minor improvements

--- lab2/tests/test_expected_value.py
+++ lab2/tests/test_expected_value.py
@@ -1,14 +1,5 @@
 import numpy as np
-
-
-def expected_value(weights):
-    """
-    Function calculate the means vector.
-    :param weights: perceptron weights
-    :return: means vector
-    """
-    return np.linalg.solve(a=weights[:4].reshape((2, 2)),
-                           b=-0.5 * weights[4:6])
+from ..ellipse import expected_value
 
 
 def test_expected_value():
--- lab2/tests/test_sample_generator.py
+++ lab2/tests/test_sample_generator.py
@@ -1,19 +1,4 @@
-import numpy as np
-
-
-def sample_generator(bias, n=50):
-    """
-    Sample_generator generate sample from multivariate normal distribution
-    with given params
-    :param bias: bias in mean
-    :param n: number of sample to generate
-    :return: sample
-    """
-    mean = np.random.rand(2) + 2 * [bias]
-    a = np.random.rand(2, 2)
-    cov = np.dot(a, a.transpose())
-
-    return np.random.multivariate_normal(mean, cov, n)
+from ..ellipse import sample_generator
 
 
 def test_sample_generator():
